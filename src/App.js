c66052d0c63fe25d0939e122056b5d4fbf9c0123
Ryan <tjddnfkdls0@naver.com>
2024-09-04 14:59:58 +0900

    주석처리 ~ onDragEnd 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useCallback, useEffect } from "react";
-import ReactDOM from "react-dom";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import styled from "styled-components";
 import { AddItemButton, DraggableItem } from "./atomic/molecules/ListItem";
@@ -19,8 +18,6 @@ const ContainerWrap = styled.div`
   overflow: auto;
   background: #F5F5F5;
 `
-
-
 
 const SumOfSelectedItems = styled.div`
   position: absolute;
@@ -81,14 +78,14 @@ const SaveButton = styled.button`
   /* margin-left: 12px; */
 `
 
-
 function App() {
-
-  // 조건문이 복잡해서 
+  // 조건문이 복잡해서 따로 style을 return 하는 getItemStyle() 사용. useCallback의 사용 이유는 성능 최적화 때문.
+  // getItemStyle()은 복잡한 조건문이 여러개 있고, 렌더링 시 반복적으로 호출될 수 있기 때문에
+  // 마지막인자 []은 의존성배열이므로 해당 함수는 컴포넌트의 생애 주기에 한번만 생성됨
   const getItemStyle = useCallback((basicStyle, selectedItemIds, itemId, currentDraggingId, itemBlocked, containerBlocked) => {
     if (currentDraggingId == itemId && containerBlocked) {
       return {
-        ...basicStyle,
+        ...basicStyle, // 여기서 basicStyle은 beautiful dnd가 기본적으로 제공하는 스타일을 사용함 (provided.draggableProps.style을 받아옴)
         background: '#FFCDD2',
         border: '1px solid #EF9A9A',
         opacity: 0.9,
@@ -111,8 +108,8 @@ function App() {
         opacity: 0.9,
       }
     }
-    if (selectedItemIds.includes(itemId)) {
-      if (currentDraggingId && currentDraggingId !== itemId) {
+    if (selectedItemIds.includes(itemId)) { // 다중선택한 아이템이 컬럼아이템에 존재할경우
+      if (currentDraggingId && currentDraggingId !== itemId) { //드래그한 아이템이 존재하며, 다중 선택 아이템중 드래그한 아이템이 아닐경우
         return {
           ...basicStyle,
           background: '#B3E5FC',
@@ -132,11 +129,12 @@ function App() {
   }, [])
 
 
+  // 아이템 배열 생성
   const getItems = (num, count) =>
     Array.from({ length: count }, (num, k) => k).map((k) => ({
-      id: generateColumnItemId(),
+      id: generateColumnItemId(), // 유니크한 key값을 가지도록 id생성
       content: `List${num} Item${k + 1}`,
-      number: k + 1
+      number: k + 1 // 짝수인지, 홀수인지 판단하기 위해 number라는 키를 부여함. 0부터 시작하는 인덱스를 1부터 시작시키도록 +1
     }));
 
   // 단일 아이템 추가
@@ -150,155 +148,159 @@ function App() {
 
   const [containerBlocked, setContainerBlocked] = useState(false) // 첫번째 컬럼 -> 세번째 컬럼 block
   const [itemBlocked, setItemBlocked] = useState(false) // 아이템 이동 불가 block
-  const [selectedItemIds, setSelectedItemIds] = useState([]) // 다중 선택시 선택된 아이템
+  const [selectedItemIds, setSelectedItemIds] = useState([]) // 다중 선택시 선택된 아이템의 Ids
   const [currentColumn, setCurrentColumn] = useState(null)
   const [currentDraggingId, setCurrentDraggingId] = useState('')
+  const [columns, setColumns] = useState([])
 
-  const [toastText, setToastText] = useState(null)
-  
+  const [toastText, setToastText] = useState(null) // 데이터 저장 및 초기화 할때 메세지를 띄워주는 용도. toastText값으로 메시지를 표기 ({toastText && (<Toast>{toastText}</Toast>)})
 
   const openToast = (text) => {
-    setToastText(text)
-    setTimeout(() => setToastText(null), 2000)
+    setToastText(text) // 토스트 메시지 상태 업데이트
+    setTimeout(() => setToastText(null), 2000) // 토스트 메시지 2초 후 삭제 -> null
   }
-  
-  const [columns, setColumns] = useState([])
+
   
   const onClickSaveButton = () => {
-    window.localStorage.setItem(SAVED_COLUMN_DATA, JSON.stringify(columns))
-    openToast('데이터가 저장되었습니다')
+    window.localStorage.setItem(SAVED_COLUMN_DATA, JSON.stringify(columns)) // SAVED_COULMN_DATA라는 key값으로 localStorage에 columns데이터를 JSON으로 변환시켜서 저장한다
+    openToast('데이터가 저장되었습니다') // 데이터 저장시 보여 줄 토스트 문구
   }
 
   const onClickResetButton = () => {
-    window.localStorage.removeItem(SAVED_COLUMN_DATA)
-    setupDefaultColumns()
-    openToast('데이터가 초기화되었습니다')
+    window.localStorage.removeItem(SAVED_COLUMN_DATA) // 저장한 데이터를 localStorage에서 키값 삭제
+    setupDefaultColumns() // 초기 컬럼값으로 되돌림
+    openToast('데이터가 초기화되었습니다') // 초기화 토스트 문구
   }
 
-  const onDragEnd = (result) => {
+  // 드래그를 Drop했을때 발생하는 이벤트
+  const onDragEnd = (result) => { // 라이브러리에서 result라는 인자를 통해 destination, source, draggableId를 제공함
     setCurrentDraggingId(null)
-    if (!result.destination || !result.source) {
+    if (!result.destination || !result.source) { // 언제나 예외처리, 데이터가 없을 경우 페이지가 터지니까
       return;
     }
 
     const {
       destination,
       source,
-      draggableId
-    } = result
-
-    const { droppableId: destDroppableId, index: destIndex } = destination
-    const { droppableId: sourceDroppableId, index: sourceIndex } = source
+      draggableId //드래그 하는 아이템의 id에 해당함
+    } = result // 구조분해할당으로 result 객체의 변수 선언 (편의성)
+    console.log(result)
+    const { droppableId: destDroppableId, index: destIndex } = destination // 마찬가지로 구조분해할당으로 destination, source가 가진 객체 변수 선언 (편의성)
+    const { droppableId: sourceDroppableId, index: sourceIndex } = source // 구조분해할당
 
     // setSelectedItems([]) // drop후 다중선택 초기화
 
-    // 1. 같은 DroppableId 내에 있는 경우
-    // 1-1. 기존 방식의 splice를 이용한 setItem()
-    if (sourceDroppableId === destDroppableId) {
+    // 같은 DroppableId 내에 있는 경우
+    if (sourceDroppableId === destDroppableId) { // column의 id값에 해당함
+      const newColumns = [...columns] // 얕은 복사를 통해 새로운 변수를 만들었음. 하지만 결과적으로 columns내부 데이터들이 바뀔거라 원본 데이터를 보호하지 못함.
+                                      //  애초에 이 프로젝트에서 원본데이터 유지의 중요성조차 없음. 깊은 복사까지 갈 필요도 없고, 얕은 복사조차 사실 무의미함.
+                                      // 그렇다면 왜? 얕은 복사를 진행한것인가? => "불변성을 지키려는 개발자의 일종의 관습에 불과함."
 
-      const newColumns = [...columns]
-      const sourceColumn = newColumns.find(column => column.id === sourceDroppableId)
+      const sourceColumn = newColumns.find(column => column.id === sourceDroppableId) // 드래그를 Drop했을 때 컬럼아이디가 같은 source컬럼을 찾아옴
       if (!sourceColumn) {
         alert('cannot find target column')
-        return;
+        return; // 예외처리
       }
 
-      // 5.같은 컬럼에서 다중 아이템이동 (2개 이상)
+      // 같은 컬럼에서 다중 아이템 이동 (2개 이상)
       if (selectedItemIds.length > 1) {
 
-        const finalDestIndex = sourceIndex < destIndex ? destIndex + 1 : destIndex
-        // 1. 선택된 아이템을 가져오고 나머지 아이템으로 구성된 Array를 만들어준다.(+ sorting)
+        const finalDestIndex = sourceIndex < destIndex ? destIndex + 1 : destIndex // sourceIndex < destIndex 경우(드래그를 아래로) 한칸씩 뒤의 아이템들이 앞당겨지므로 destIndex + 1을 해야함
+                                                                                   // sourceIndex > destIndex 경우(드래그를 위로) 아이템들의 인덱스 변화 x
+                                                                                   // 그렇다면 sourceIndex < destIndex일때 단일 아이템은 왜 destIndex + 1처리를 안하는가? => 단일 아이템은 하나의 아이템이 제거되고 정확한 위치에 채워지기때문에 인덱스 조정이 필요없음
+
         const sourceItems = sourceColumn.items
-        // 선택된 아이템 변수
-        const selectedItems = []
-        // 선택된 아이템 이외의 아이템을 위한 변수
+        
+        const selectedItems = [] // 선택된 아이템 변수
         const filteredSourceItems = sourceItems.filter(item => {
           if (selectedItemIds.includes(item.id)) {
-            selectedItems.push(item)
+            selectedItems.push(item)         
             return false
           } else {
-            return true
+            return true       // 선택된 아이템 이외의 아이템을 위한 변수
           }
         })
+
         // 선택된 sorting
-        let draggableItem
-        const sortedSelectedItems = []
-
+        let draggableItem // drag하는 아이템 변수 선언
+        const sortedSelectedItems = [] // 다중 선택 아이템 변수 선언
         selectedItems.forEach(item => {
-          if (item.id === draggableId) {
-            draggableItem = { ...item, moved: true }
+          if (item.id === draggableId) { 
+            draggableItem = { ...item, moved: true } // 다중 선택 중 드래그 하는 아이템 변수 할당 (...item을 쓴거는 좋은 관습 원본 배열 보호 목적의 관습), moved : true라는 이동하는 아이템이라는 속성을 임의로 부여
           } else {
-            sortedSelectedItems.push({ ...item, moved: true })
+            sortedSelectedItems.push({ ...item, moved: true }) // 드래그에 해당하지 않는 나머지 아이템들
+
           }
         })
-        sortedSelectedItems.unshift(draggableItem)
+        sortedSelectedItems.unshift(draggableItem) // unshift를 사용함으로써 배열의 순서에 draggableItem이 최우선으로 당겨짐
 
-        // 짝수 아이템 이동불가 처리
+        // 다중 아이템 이동 중 짝수 아이템 이동불가 처리
         if (draggableItem?.number % 2 === 0 &&
-          sourceItems[finalDestIndex]?.number % 2 === 0
+          sourceItems[finalDestIndex]?.number % 2 === 0 // 아이템의 number속성을 기준으로 홀수, 짝수 판별해서 드래그한 아이템과 drop하는 자리의 아이템의 number를 비교
         ) {
-          setItemBlocked(false)
+          setItemBlocked(false)   // 이건 드래그하는 중에 예외 css를 입히기 위한것으로 onDragEnd할때는 false로 바꿔줘야함
 
-          setSelectedItemIds([])
-          return;
+          setSelectedItemIds([]) // 다중 선택 배열 초기화
+          return; // 실행 중단
         }
 
-        sourceItems.splice(finalDestIndex, 0, ...sortedSelectedItems)
-        sourceColumn.items = sourceItems.reduce((acc, item) => {
-          if (selectedItemIds.includes(item.id)) {
-            if (!item.moved) {
+        // 다중 아이템 이동
+        sourceItems.splice(finalDestIndex, 0, ...sortedSelectedItems) // finalDestIndex자리에 다중선택 아이템들 추가시킴 (교체가 아닌, 추가의 개념)
+
+        sourceColumn.items = sourceItems.reduce((acc, item) => { // 기존 선택한 아이템은 이동했다고보고 필터링을 해야함
+          if (selectedItemIds.includes(item.id)) { // reduce메서드에서 요소들이 한번씩 돌때, 다중선택한 아이템id에 포함될때 moved속성이 없다면 (기존배열) return acc; 그냥 pass (acc에 추가 안함)
+            if (!item.moved) {                    
+              console.log(acc)
               return acc
             }
-            delete item.moved
+            delete item.moved                      // moved속성이 있을경우(다중 선택한 아이템배열) moved속성은 더이상 필요없으니 삭제 -> acc에 push
           }
-          acc.push(item)
+          acc.push(item)                           // 다중 선택에 해당하지 않는 요소들은 그냥 push
           return acc
-        }, [])
-        setColumns(newColumns)
-        setSelectedItemIds([])
+        }, [])                                     // acc의 초깃값은 빈 배열로 시작. 다중 아이템의 이동이 완료되고 sourceColumn.items의 배열을 새로 교체
+        setColumns(newColumns)                     // 모든 최상위 state인 컬럼을 최종적으로 update, 사실 이미 원본배열이 바뀌어서(splice, reduce) 없어도 되지만 안정성의 보장 및 관습적으로 꼭 해줘야함
+        setSelectedItemIds([])                     // 다중이동 끝난 후에 다중 선택state 초기화
 
-        return;
-      } else {
-        const sourceItems = sourceColumn.items
-        const splicedItems = sourceItems.splice(sourceIndex, 1) // 자료구조 어레이
-        const destColumn = newColumns.find(column => column.id === destDroppableId);
+        return;                                    // 함수의 종료 명시, 사실 이후의 로직이 없어서 return; 표기하지 않아도 됨. 하지만 명확성을 위해 표기
+      } 
+      // 동일 컬럼 이동 내에 다중 이동이 아닌경우, 단일 이동
+      else {        
+        const sourceItems = sourceColumn.items  
+        const splicedItems = sourceItems.splice(sourceIndex, 1) // sourceIndex(선택인덱스) 자리의 1개의 요소 제거, 새로운 배열에 할당
+        const destColumn = newColumns.find(column => column.id === destDroppableId); // 동일 컬럼이라 사실상 sourceColumn 으로 처리해도 무방
         const destItems = destColumn.items
         if (!destColumn) {
-          return;
+          return; // 예외처리
         }
 
         // console.log(destItems.length) // 실제 길이는 10이지만, 9가 나오는 이유 : splicedItems로 인해 배열의 변화가 일어남
-        // 4-1. 짝수 아이템은 다른 짝수 아이템 앞으로 이동 불가
-        if (splicedItems[0].number % 2 === 0 &&
-          destIndex < destItems.length &&  // destItems.length가 줄어들었기 때문에 마지막 인덱스 이상으로 접근 못하게 함
-          destItems[destIndex].number % 2 === 0
 
+        // 단일 아이템 이동에서의 짝수 아이템은 다른 짝수 아이템 앞으로 이동 불가
+        if (splicedItems[0].number % 2 === 0 &&
+          destIndex < destItems.length &&  // destItems.length가 줄어들었기 때문에(splicedItems) 마지막 인덱스 이상으로 접근 못하게 함
+          destItems[destIndex].number % 2 === 0
         ) {
           setItemBlocked(false) // drag끝난 뒤 state초기화
-          sourceItems.splice(sourceIndex, 0, splicedItems[0]) //splicedItems는 원본 배열(newColums)에서 빠진 상태. splice()는 원본을 수정한다.
+          sourceItems.splice(sourceIndex, 0, splicedItems[0]) // 짝수 아이템 예외처리에서 다시 원본 배열상태로 되돌려놓아야함. 이미 원본 배열이 수정됐기 때문
           return; // 따라서 이 상태에서 배열 복구 없이 return만 한다면 원본 배열에서 splicedItems이 사라진 상태로 업데이트됨.
         }
 
-
-        sourceItems.splice(destIndex, 0, splicedItems[0])
-        setColumns(newColumns)
-        setSelectedItemIds([])
+        sourceItems.splice(destIndex, 0, splicedItems[0]) // 제약 조건 없이 최종 단일 아이템의 이동, destIndex에 아무런 요소를 제거하지 않고(0) 아까 제외시킨 아이템의 추가
+        setColumns(newColumns) // 최종 colums상태 업데이트
+        setSelectedItemIds([]) // 단일 아이템을 선택후(선택해도 다중아이템 state에 한개 추가됨) 이동하는 경우에도 이동이 끝나고 어쨌든 비워줘야함
 
       }
     }
 
-    // 2. 다른 DroppableId 간의 이동일 경우
-    // if(result.source.droppableId != result.)
-    // 2-1. source의 DroppableId에서 source 제거, 및 재배열(index - 1)
-    // 2-2
-    // 2-3. destination의 DroppableId에서 destination.index자리에 source추가 및 재배열(index + 1)
+
+    // 다른 컬럼(DroppableId) 간의 이동일 경우
     else if (destDroppableId !== sourceDroppableId) {
-      // 3. 첫번째 컬럼에서 세번째 컬럼으로 이동 불가
-      if (columns.length >= 3) {
+      // 첫번째 컬럼에서 세번째 컬럼으로 이동 불가 제약 조건 처리
+      if (columns.length >= 3) { // 먼저 컬럼의 갯수가 최소 3개이상이어야함
         if (sourceDroppableId == columns[0].id && destDroppableId == columns[2].id) {
           setContainerBlocked(false) // drag끝난 뒤 state 초기화
-
-          return; //이 부분은 단순 조건 검사이기 때문에 특정 조건에서 drop을 아예 차단하고 있기 때문
+          setSelectedItemIds([]) // 다중아이템일 경우 state 초기화
+          return; // 이 부분은 단순 조건 검사이기 때문에 특정 조건에서 drop을 아예 차단하고 있기 때문
         }
       }
 
@@ -309,18 +311,17 @@ function App() {
         return;
       }
 
-      const sourceItems = [...sourceColumn.items]
+      const sourceItems = sourceColumn.items
       const destColumn = newColumns.find(column => column.id === destDroppableId)
       if (!destColumn) {
         return;
       }
-      const destItems = [...destColumn.items]
+      const destItems = destColumn.items
 
 
-      // 5.1 다른컬럼으로 다중아이템 이동 (2개이상)
+      // 다른컬럼으로 다중아이템 이동 (2개이상)
       if (selectedItemIds.length > 1) {
         const finalDestIndex = sourceIndex < destIndex ? destIndex + 1 : destIndex
-        // 1. 선택된 아이템을 가져오고 나머지 아이템으로 구성된 Array를 만들어준다.(+ sorting)
         // 선택된 아이템 변수
         const selectedItems = []
         // 선택된 아이템 이외의 아이템을 위한 변수
@@ -335,34 +336,32 @@ function App() {
         // 선택된 sorting
         let draggableItem
         const sortedSelectedItems = selectedItems.filter(item => {
-          if (item.id === draggableId) {
-            draggableItem = { ...item };
+          if (item.id === draggableId) { 
+            draggableItem = { ...item };  // 여기서는 moved라는 속성을 주지 않았음, 배열의 변화는 sourceColumn이 아닌 destColumn에서 일어날것이기때문에 
             return false;
           }
           return true;
         });
 
-        sortedSelectedItems.unshift(draggableItem)
+        sortedSelectedItems.unshift(draggableItem) // 다중 선택 배열의 정렬
 
-        // // 짝수 아이템 이동불가 처리
+        // 다중 아이템 이동 중 짝수 아이템 이동불가 처리
         if (draggableItem?.number % 2 === 0 &&
           destItems[destIndex]?.number % 2 === 0
         ) {
           setItemBlocked(false)
           setSelectedItemIds([])
-
           return;
         }
 
 
-        destItems.splice(finalDestIndex, 0, ...sortedSelectedItems)
-        sourceColumn.items = filteredSourceItems
-        destColumn.items = destItems
-
+        destItems.splice(finalDestIndex, 0, ...sortedSelectedItems) // destItems배열에 다중 선택 아이템들 추가
+        sourceColumn.items = filteredSourceItems // 동일 컬럼에서의 다중 이동과 다르게 여기서는 sourceColumn을 선택되지 않았던 아이템들로 단순 배열 교체시킴
         setColumns(newColumns)
         setSelectedItemIds([])
         return;
       }
+      // 다른 컬럼간 이동 중 단일 이동
       else {
         const newColumns = [...columns]
         const sourceColumn = newColumns.find(column => column.id === sourceDroppableId)
@@ -380,13 +379,11 @@ function App() {
 
         const destItems = destColumn.items
 
-        // 4-2. 짝수 아이템은 다른 짝수 아이템 앞으로 이동 불가
+        // 짝수 아이템은 다른 짝수 아이템 앞으로 이동 불가
         if (splicedItems[0].number % 2 === 0 &&
           destIndex < destItems.length &&  // destItems.length가 줄어들었기 때문에 마지막 인덱스 이상으로 접근 못하게 함
           destItems[destIndex].number % 2 === 0) {
           sourceItems.splice(sourceIndex, 0, splicedItems[0]) //splicedItems는 원본 배열(newColums)에서 빠진 상태. splice()는 원본을 수정한다.
-          setSelectedItemIds([])
-
           return; // 따라서 이 상태에서 배열 복구 없이 return만 한다면 원본 배열에서 splicedItems이 사라진 상태로 업데이트됨.
         }
 
@@ -399,27 +396,27 @@ function App() {
     }
   }
 
+  // 드래그를 시작했을때 처음 발생하는 이벤트
   const onDragStart = (start) => {
     setContainerBlocked(false)
     setItemBlocked(false)
 
     if (!start.source) {
-      return
+      return  // 예외처리
     }
 
     const {
       source,
       draggableId
-    } = start
+    } = start // 구조분해 할당
 
-    const { droppableId: sourceDroppableId, index: sourceIndex } = source
+    const { droppableId: sourceDroppableId, index: sourceIndex } = source // 구조분해할당으로 선언해놨지만 안썼음
 
-    setCurrentDraggingId(draggableId)
-
+    setCurrentDraggingId(draggableId) // 드래그 중인 아이템의 id를 state에 저장
   }
 
 
-  // 드래그 중 예외처리 css처리 함수
+  // 드래그 중(ing) 발생하는 이벤트,  예외처리 css처리 함수
   const onDragUpdate = (result) => {
     if (!result.destination || !result.source) {
       return;
@@ -520,6 +517,7 @@ function App() {
     setColumns(newColumns)
   }
 
+  // 초기 Columns값 설정
   const setupDefaultColumns = () => {
     resetAllSets()
     setColumns([
@@ -544,15 +542,16 @@ function App() {
       }
     ])
   }
+
+  // 로컬스토리지에 저장한 columns값을 불러오기 위한 함수
   const initializeColumns = () => {
     try {
       const savedColumnStr = window.localStorage.getItem(SAVED_COLUMN_DATA)
       if (!savedColumnStr || savedColumnStr === '') {
-        throw new Error('저장된 데이터가 없습니다')
+        throw new Error('저장된 데이터가 없습니다') // 현재 이 error는 try catch로 처리되어 catch에 있는 내용이 아니라 출력이 되지않고있음
       }
-        
-      
-      const columns = JSON.parse(savedColumnStr)
+
+      const columns = JSON.parse(savedColumnStr) //JSON형식의 value값을 pasing해서 객체나 배열로 변환 
       const columnIds = []
       const columnItemIds = []
       columns.forEach(column => {
@@ -563,17 +562,20 @@ function App() {
         columnIds.push(id)
         items.forEach(item => columnItemIds.push(item.id))
       })
+      // columnIds와 columnItemIds를 Set 자료형으로 추가 (후에 추가될 다른 id들과 중복을 피하기 위함)
       insertColumnIds(columnIds)
       insertColumnItemIds(columnItemIds)
-      setColumns(columns)
-      
+      setColumns(columns) //최종적으로 columns 상태 업데이트
+
     } catch (e) {
-      setupDefaultColumns()
+      setupDefaultColumns() //저장된 값이 없으면 초기 컬럼값 설정
     }
   }
+
+  // 처음 랜더링할때 한번 실행
   useEffect(() => {
     initializeColumns()
-    
+
   }, [])
 
   return (
@@ -583,11 +585,11 @@ function App() {
           Drag & Drop Item Management
         </Title>
         <div>
-        <SaveButton onClick={onClickResetButton}>초기화</SaveButton>
-        <SaveButton
-         onClick={onClickSaveButton}
-         style={{background : '#00796b', color : 'white'}}
-         >저장</SaveButton>
+          <SaveButton onClick={onClickResetButton}>초기화</SaveButton>
+          <SaveButton
+            onClick={onClickSaveButton}
+            style={{ background: '#00796b', color: 'white' }}
+          >저장</SaveButton>
         </div>
       </Header>
       <ContainerWrap>
--- src/utils/uuid.js
+++ src/utils/uuid.js
@@ -1,6 +1,11 @@
+
+// Set객체는 중복 값을 허용하지 않음, 따라서 Column or Item의 Id를 효율적으로 저장 가능
+// Set객체에는 기본 메서드로 (add, delete, clear, has) 존재
 export const columnIdSet = new Set()
 export const columnItemIdSet = new Set()
 
+// Column에 고유한 key값을 부여하기 위해 유니크한 아이디를 랜덤해서 생성
+// 생성한 아이디가 이미 존재할 경우 함수를 재실행
 export const generateColumnId = () => {
   const uuid = 'column' + parseInt(Math.random() * 100000)
   if (columnIdSet.has(uuid)) {
@@ -10,8 +15,10 @@ export const generateColumnId = () => {
   return uuid
 }
 
+// Item에 고유한 key값을 부여하기 위해 유니크한 아이디를 랜덤해서 생성
+// 생성한 아이디가 이미 존재할 경우 함수를 재실행
 export const generateColumnItemId = () => {
-  const uuid = 'item' + parseInt(Math.random() * 100000)
+  const uuid = 'item' + parseInt(Math.random() * 100000) //Math.random()은 0이상 1미만의 난수를 생성하고, parseInt로 인해 정수로 변환
   if (columnItemIdSet.has(uuid)) {
     return generateColumnItemId()
   }
@@ -19,15 +26,20 @@ export const generateColumnItemId = () => {
   return uuid
 }
 
+
+// id중복검사
 export const insertColumnIds = (columnIds) => {
     columnIds.forEach(id => columnIdSet.add(id))
 }
 
+// id중복검사
 export const insertColumnItemIds = (columnItemIds) => {
     columnItemIds.forEach(id => columnItemIdSet.add(id))
     
 }
 
+
+// id초기화 (columns state 초기값 설정시)
 export const resetAllSets = () => {
     columnIdSet.clear()
     columnItemIdSet.clear()
